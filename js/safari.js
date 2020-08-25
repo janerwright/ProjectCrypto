afab27504ec16463e7ec981adfffcd1c2fde1dde
Harry S <HarryIsSecured@pm.me>
2020-08-25 18:01:52 +0100

    Added Safari CSS targeting
    Added a JS file which will target Safari browsers and change the style sheet so that the website looks good across all major browsers, now including Safari

---
+++ js/safari.js
@@ -0,0 +1,4 @@
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+    document.getElementById('stylesheet').href = '../css/mainsafari.css';
+}
+// https://www.denisbouquet.com/javascript-targeting-safari-only/
